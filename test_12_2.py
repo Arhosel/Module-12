c07c42c4c3c4f5650b8563140f7660e32705b9cf
Alex <144250961+Arhosel@users.noreply.github.com>
2025-01-04 23:11:48 +0500

    Update test_12_2.py

--- test_12_2.py
+++ test_12_2.py
@@ -1,58 +1,62 @@
+import runner_and_tournament as rt
 import unittest
 
-import Tournament
-
-from runner_and_tournament import Runner
-
-# class Runner:
-#     def __init__(self, name, speed):
-#         self.name = name
-#         self.speed = speed
-#
-# class Tournament:
-#     def __init__(self, distance, *runners):
-#         self.distance = distance
-#         self.runners = runners
-#
-#     def start(self):
-#         results = {}
-#         for runner in self.runners:
-#             time = self.distance / runner.speed
-#             results[time] = runner.name
-#         return dict(sorted(results.items()))
 
 class TournamentTest(unittest.TestCase):
+
+#тестовый класс "TournamentTest`, который наследуется от "unittest".TestCase`. Этот класс содержит модульные тесты для моделирования турнира
     @classmethod
     def setUpClass(cls):
         cls.all_results = {}
-
+#Это метод класса, который запускается один раз перед выполнением всех тестов.
+#Он инициализирует переменную класса `all_results` как пустой словарь для хранения результатов всех тестов
     def setUp(self):
-        self.usain = Runner("Усэйн", 10)
-        self.andrey = Runner("Андрей", 9)
-        self.nick = Runner("Ник", 3)
-
+        self.runer_1 = rt.Runner('Усейн', 10)
+        self.runer_2 = rt.Runner('Андрей', 9)
+        self.runer_3 = rt.Runner('Ник', 3)
+#Этот метод запускается перед каждым тестом.
+#Он создает три объекта "Runner" с разными именами и скоростями
     @classmethod
     def tearDownClass(cls):
-        for time, name in cls.all_results.items():
-            print(f"{time}: {name}")
+        for test_key, test_value in cls.all_results.items():
+            print(f'Тест: {test_key}')
+            for key, value in test_value.items():
+                print(f'\t{key}: {value.name}')
+#Этот метод класса запускается один раз после завершения всех тестов.
+#Он выводит результаты, сохраненные в "all_results", с указанием имени каждого участника и его позиции для каждого теста.
+    def test_turn1(self):
 
-    def test_usain_and_nick(self):
-        tournament = Tournament(90, self.usain, self.nick)
-        results = tournament.start()
-        TournamentTest.all_results.update(results)
-        self.assertTrue(list(results.values())[-1] == "Ник")
+        # list_test = [[self.runer_1, self.runer_3], [self.runer_2, self.runer_3],
+        #              [self.runer_1, self.runer_2, self.runer_3]]
+        turn_1 = rt.Tournament(90, self.runer_1, self.runer_3)
+        result = turn_1.start()
+        # print(result[list(result.keys())[-1]] == 'Ник')
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ошибка! Последним должен быть Ник')
+        self.all_results['test_turn1'] = result     # test_turn
 
-    def test_andrey_and_nick(self):
-        tournament = Tournament(90, self.andrey, self.nick)
-        results = tournament.start()
-        TournamentTest.all_results.update(results)
-        self.assertTrue(list(results.values())[-1] == "Ник")
+    def test_turn2(self):
+        turn_2 = rt.Tournament(90, self.runer_2, self.runer_3)
+        result = turn_2.start()
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ошибка! Последним должен быть Ник')
+        self.all_results['test_turn2'] = result
 
-    def test_usain_andrey_and_nick(self):
-        tournament = Tournament(90, self.usain, self.andrey, self.nick)
-        results = tournament.start()
-        TournamentTest.all_results.update(results)
-        self.assertTrue(list(results.values())[-1] == "Ник")
+    def test_turn3(self):
+        turn_3 = rt.Tournament(90, self.runer_1, self.runer_2, self.runer_3)
+        result = turn_3.start()
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ошибка! Последним должен быть Ник')
+        self.all_results['test_turn3'] = result
+#Каждый метод создает объект `Турнир` с дистанцией в 90 единиц и различными комбинациями участников.
+#Вызывается метод `start()` турнира, и результат сохраняется.
+#Утверждение проверяет, является ли 'Nik' (ник) последним участником в каждом турнире.
+#Результаты сохраняются в словаре `all_results`.
 
-if __name__ == '__main__':
-    unittest.main()
+    # def test_turn4(self):
+    #     #тестирует более короткую гонку (6 этапов) со всеми тремя бегунами
+    #     turn_4 = rt.Tournament(6, self.runer_1, self.runer_2, self.runer_3)
+    #     result = turn_4.start()
+    #     self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ошибка! Последним должен быть Ник')
+    #     self.all_results['test_turn4'] = result
+
+    if __name__ == '__main__':
+        unittest.main()
+    # Запускает все тесты
